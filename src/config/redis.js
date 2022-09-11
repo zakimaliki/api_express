7ee2fa6c5d629a8dc3ea7d90c7eb93107f377153
zakimaliki-macbook <Zaki@MacintoshHDsAir.lan>
2022-09-11 17:02:53 +0700

    new

--- src/config/redis.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('redis');
-
-const client = createClient(6379);
-
-client.on('error', (err) => console.log('Redis Client Error', err));
-
-client.connect();
-
-module.exports = client
